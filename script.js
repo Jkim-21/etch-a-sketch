cfa670296592e5bfa276aa10a0eb38c5128971f7
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-11-29 18:05:50 -0500

    Add "hover" effect to paint the grid when the mouse passes over it.
    This functionality serves the main purpose of "etch-a-sketch",
    allowing users to paint onto the grid when hovering over the cells.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 
 let gridContainer = document.querySelector(".gridContainer");
 let squaresPerSide = 16;
+let startDrawing = false;
+let drawColor = "black";
 
 createGrid(squaresPerSide);
+draw();
 
 function createGrid(squares) {
     for (let i = 0; i < squares; i++) {
@@ -16,5 +19,24 @@ function createGrid(squares) {
             cell.classList.add("cell");
             row.appendChild(cell);
         }
+    }
+}
+
+function draw() {
+    let squares = document.querySelectorAll(".cell");
+    squares.forEach(square => {
+        square.addEventListener('click', toggleDrawing);
+        square.addEventListener('mouseover', placeColor);
+
+    })
+}
+
+function toggleDrawing() {
+    startDrawing = !startDrawing;
+}
+
+function placeColor(e) {
+    if (startDrawing) {
+        e.target.style.backgroundColor = drawColor;
     }
 }
